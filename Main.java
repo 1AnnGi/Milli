a5f12647da1239dbe2f438c781637c5084e5c55d
AnnGi <anastasiya.larionova.11.11.97@gmail.com>
2021-12-07 19:38:44 +0300

    Save

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 10500;
+        int bonusPrice = 20;
+        int bonusMiles = ticketPrice / bonusPrice;
+        System.out.println(bonusMiles);
+    }
+}
